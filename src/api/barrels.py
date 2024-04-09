ed331283751eea9b73b7734e4ff9b2dc4e4bf797
Kieran McNulty <kmcnulty@Kierans-Laptop.local>
2024-04-08 21:57:58 -0700

    barrels.py ocmplete

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/barrels",
@@ -19,21 +21,53 @@ class Barrel(BaseModel):
 
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
-    """ """
-    print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
+    
+    with db.engine.begin() as connection:
+        output=""
+        for barrel in barrels_delivered:
+                if barrel.potion_type == [0]: # temp value
+                    gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory"))
+                    gold = gold.scalar()
 
-    return "OK"
+                    if gold >= barrel.price:
+                        new_gold = gold - barrel.price
+                        
+                        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = :gold"), {'gold': new_gold})
+
+                        ml_query = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory"))
+                        total_ml = ml_query.scalar()
+                        new_total_ml = total_ml + (barrel.ml_per_barrel * barrel.quantity)
+
+                        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = :num_green_ml"), {'num_green_ml': new_total_ml})
+                        output = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory"))
+    return output
+    
 
 # Gets called once a day
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
     print(wholesale_catalog)
+    for barrel in wholesale_catalog:
+        with db.engine.begin() as connection:
+            potion_type = barrel.potion_type #not needed yet, we only have green poitions
+            sku = barrel.sku
+            price = barrel.price
 
-    return [
-        {
-            "sku": "SMALL_RED_BARREL",
-            "quantity": 1,
-        }
-    ]
+            sql_to_execute = "SELECT gold FROM global_inventory"
+            result = connection.execute(sqlalchemy.text(sql_to_execute))
+            gold = result.scalar()
+
+            #sql_to_execute = f"SELECT num_green_potions FROM global_inventory WHERE potion_type = {potion_type}" #not needed yet
+            sql_to_execute = "SELECT num_green_potions FROM global_inventory"
+            quantity = connection.execute(sqlalchemy.text(sql_to_execute))
+            num_potions = quantity.fetchone()[0]
+            to_purchase = []
+            if num_potions <= 10 and gold >= price:
+                gold -= price #just for checking in loop if theres multiple kinds of buckets, actual gold will be changed in deliver
+                to_purchase.append({
+                    "sku": sku,
+                    "quantity": 1
+                })
+        return to_purchase
 
