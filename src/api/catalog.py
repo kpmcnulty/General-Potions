50763e7eb2a245dedfc0bbfbb699b89bb49fc1eb
Kieran McNulty <kmcnulty@Kierans-Laptop.local>
2024-04-08 23:43:48 -0700

    added catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -10,12 +10,14 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
-
+    with db.engine.begin() as connection:
+        potions_response = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
+        total_potions = potions_response.scalar() 
     return [
             {
                 "sku": "GREEN_POTIONS",
                 "name": "green potion",
-                "quantity": 1,
+                "quantity": total_potions,
                 "price": 50,
                 "potion_type": [0, 0, 100],
             }
