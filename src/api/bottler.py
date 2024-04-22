ee1e1869de227cc5b56a13830ed657c806258579
Kieran McNulty <kmcnulty@Kierans-Laptop.lan>
2024-04-21 23:24:01 -0700

    implemented bottling of custom potions

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -56,47 +56,69 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     return "OK"
 
 
+def get_ml_ratio(ml_inventory):
+    total_ml = sum(ml_inventory)
+    if total_ml < 100:
+        return None
+    ratio = [ml / total_ml for ml in ml_inventory]
+    
+    adjusted_ml = [int(ratio[i] * 100 / sum(ratio)) for i in range(len(ml_inventory))]
+
+    max_ml_index = adjusted_ml.index(max(adjusted_ml))
+    while sum(adjusted_ml) < 100:
+        adjusted_ml[max_ml_index] += 1  #should be fine since we check if total ml < 100
+        
+    return adjusted_ml
+
+
 @router.post("/plan")
 def get_bottle_plan():
     """
     Go from barrel to bottle.
     """
-    potions_to_purchase = []
+    potions_to_bottle = []
     with db.engine.begin() as connection:
+        results = connection.execute(sqlalchemy.text(
+                """
+                SELECT
+                potion_capacity,
+                green_ml,
+                blue_ml,
+                red_ml,
+                dark_ml
+                FROM globals""")).one()
+        total_potions = connection.execute(sqlalchemy.text(
+            "SELECT SUM(quantity) FROM potions"
+        )).scalar()
+        if not total_potions:
+            total_potions = 0
+
+        ml_inventory = [results.red_ml, results.green_ml, results.blue_ml, results.dark_ml] 
+        total_ml = sum(ml_inventory)
         
-        green_ml_response = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory"))
-        total_green_ml = green_ml_response.scalar()
-        num_green_bottles = total_green_ml // 100
 
-        red_ml_response = connection.execute(sqlalchemy.text("SELECT num_red_ml FROM global_inventory"))
-        total_red_ml = red_ml_response.scalar()
-        num_red_bottles = total_red_ml // 100
+        ml_ratio = get_ml_ratio(ml_inventory)
+        if not ml_ratio:
+            return []
 
-        blue_ml_response = connection.execute(sqlalchemy.text("SELECT num_blue_ml FROM global_inventory"))
-        total_blue_ml = blue_ml_response.scalar()
-        num_blue_bottles = total_blue_ml // 100
+        quantity = 0
+        potions_to_bottle = []
 
+        quantities = []
+        for inventory, ratio in zip(ml_inventory, ml_ratio):    
+            if ratio != 0:
+                quantities.append(inventory // ratio)
+        quantity = min(quantities)
+        if total_potions < results.potion_capacity:
+            potions_to_bottle.append(
+                {
+                    "potion_type": ml_ratio,  
+                    "quantity": quantity,
+
+                }
+            )
         
-        
-        if num_green_bottles > 0:
-            potions_to_purchase.append({
-                "potion_type": [0, 100, 0, 0],  
-                "quantity": num_green_bottles,
-
-            })
-        if num_red_bottles > 0:
-            potions_to_purchase.append({
-                "potion_type": [100, 0, 0, 0],  
-                "quantity": num_red_bottles,
-
-            })
-        if num_blue_bottles > 0:
-            potions_to_purchase.append({
-                "potion_type": [0, 0, 100, 0],  
-                "quantity": num_blue_bottles,
-
-            })
-    return potions_to_purchase 
+    return potions_to_bottle
 
 
 if __name__ == "__main__":
