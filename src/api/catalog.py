b107ad89dc96c54fb9bee9614af4530c5dfa50b3
Kieran McNulty <kmcnulty@Kierans-Laptop.lan>
2024-05-11 21:38:50 -0700

    Fixed catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -13,28 +13,27 @@ def get_catalog():
     with db.engine.begin() as connection:
         potions = connection.execute(sqlalchemy.text("SELECT * FROM potions")).all()
 
-    catalog = []
-    counter = 0
-    for potion in potions:
-        try:
+        catalog = []
+        counter = 0
+        for potion in potions:
+            
             quantity = connection.execute(sqlalchemy.text(
-                "SELECT SUM(delta_potion) FROM potion_transacions WHERE sku = :sku",
-                {"sku": potion.sku},
-            )).scalar()
-        except Exception:
-            quantity = 0
-        if quantity == None:
-            quantity = 0
-        if potion.quantity > 0 and counter < 6:
-            catalog.append(
-            {
-                "sku": potion.sku,
-                "name": potion.name,
-                "quantity": quantity, 
-                "price": potion.price, 
-                "potion_type": potion.potion_type
-            })
-            counter += 1
+                    "SELECT SUM(delta_potion) FROM potion_transactions WHERE sku = :sku"),
+                    [{"sku": potion.sku}]
+                ).scalar()
+            if quantity == None:
+                quantity = 0
+            if quantity > 0 and counter < 6:
+                
+                catalog.append(
+                {
+                    "sku": potion.sku,
+                    "name": potion.name,
+                    "quantity": quantity, 
+                    "price": potion.price, 
+                    "potion_type": potion.potion_type
+                })
+                counter += 1
     
     return catalog
         
